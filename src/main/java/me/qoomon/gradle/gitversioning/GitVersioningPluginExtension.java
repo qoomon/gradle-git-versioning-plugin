cf59030f799e2e919975b78718a6c861cbfe02cb
Bengt Brodersen <bengt.brodersen@gmail.com>
2021-07-14 08:54:29 +0200

    refactor: optimize imports

--- src/main/java/me/qoomon/gradle/gitversioning/GitVersioningPluginExtension.java
+++ src/main/java/me/qoomon/gradle/gitversioning/GitVersioningPluginExtension.java
@@ -15,7 +15,10 @@ import org.gradle.api.Project;
 import org.gradle.api.logging.Logger;
 import org.gradle.api.plugins.ExtraPropertiesExtension;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
