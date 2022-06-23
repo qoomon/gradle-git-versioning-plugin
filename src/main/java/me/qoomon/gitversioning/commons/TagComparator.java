3208bc77f4f993e254bcb0fb4e0c7be762c404e0
Bengt Brodersen <bengt.brodersen@gmail.com>
2022-06-23 21:53:30 +0200

    fix: handle github pre-release

--- src/main/java/me/qoomon/gitversioning/commons/TagComparator.java
+++ src/main/java/me/qoomon/gitversioning/commons/TagComparator.java
@@ -1,5 +1,6 @@
 package me.qoomon.gitversioning.commons;
 
+import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.*;
@@ -7,6 +8,7 @@ import org.eclipse.jgit.revwalk.*;
 import java.io.IOException;
 import java.util.Comparator;
 import java.util.Date;
+import java.util.concurrent.Callable;
 
 import static org.eclipse.jgit.lib.Constants.OBJ_TAG;
 
@@ -20,32 +22,47 @@ public class TagComparator implements Comparator<Ref> {
 
     @Override
     public int compare(Ref ref1, Ref ref2) {
+        RevObject rev1 = tryUnchecked(() -> revWalk.parseAny(ref1.getObjectId()));
+        RevObject rev2 = tryUnchecked(() -> revWalk.parseAny(ref2.getObjectId()));
+
+        // both tags are annotated tags
+        if (rev1 instanceof RevTag && rev2 instanceof RevTag) {
+            return compareTaggerDate((RevTag) rev1, (RevTag) rev2);
+        }
+
+        // only ref1 is annotated tag
+        if (rev1 instanceof RevTag) {
+            return -1;
+        }
+
+        // only ref2 is annotated tag
+        if (rev2 instanceof RevTag) {
+            return 1;
+        }
+
+        // both tags are lightweight tags
+        return compareTagVersion(ref1, ref2);
+    }
+
+    private static <R> R tryUnchecked(Callable<R> block) {
         try {
-            RevTag revTag1 = revWalk.parseTag(ref1.getObjectId());
-            RevTag revTag2 = revWalk.parseTag(ref2.getObjectId());
-
-            // both tags are annotated tags
-            if (revTag1.getType() == OBJ_TAG && revTag2.getType() == OBJ_TAG) {
-                Date revTag1Date = revTag1.getTaggerIdent().getWhen();
-                Date revTag2Date = revTag2.getTaggerIdent().getWhen();
-                // most recent tags first
-                return -revTag1Date.compareTo(revTag2Date);
-            }
-
-            // only ref1 is annotated tag
-            if (revTag1.getType() == OBJ_TAG) {
-                return -1;
-            }
-
-            // only ref2 is annotated tag
-            if (revTag2.getType() == OBJ_TAG) {
-                return 1;
-            }
-
-            // both tags are lightweight tags
-            return revTag1.name().compareTo(revTag1.getName());
-        } catch (IOException e) {
+            return block.call();
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static int compareTagVersion(Ref ref1, Ref ref2) {
+        DefaultArtifactVersion version1 = new DefaultArtifactVersion(ref1.getName());
+        DefaultArtifactVersion version2 = new DefaultArtifactVersion(ref2.getName());
+        // sort the highest version first
+        return -version1.compareTo(version2);
+    }
+
+    private static int compareTaggerDate(RevTag rev1, RevTag rev2) {
+        Date revTag1Date = rev1.getTaggerIdent().getWhen();
+        Date revTag2Date = rev2.getTaggerIdent().getWhen();
+        // sort the most recent tags first
+        return -revTag1Date.compareTo(revTag2Date);
     }
 }
