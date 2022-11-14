56e72527bc455aeea51d8ab7bd64f88cd0de15be
Adam <897017+aSemy@users.noreply.github.com>
2022-11-14 23:27:31 +0100

    refactor tests to reduce some copy/pasting, and add test for tagged release

--- src/testFunctional/kotlin/me/qoomon/gradle/gitversioning/GitVersioningFunctionalTests.kt
+++ src/testFunctional/kotlin/me/qoomon/gradle/gitversioning/GitVersioningFunctionalTests.kt
@@ -3,22 +3,30 @@ package me.qoomon.gradle.gitversioning
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldContain
 import io.kotest.matchers.string.shouldNotContain
+import me.qoomon.gradle.gitversioning.util.GradleGroovyProjectTest
 import me.qoomon.gradle.gitversioning.util.GradleGroovyProjectTest.Companion.gradleGroovyProjectTest
+import me.qoomon.gradle.gitversioning.util.GradleKtsProjectTest
 import me.qoomon.gradle.gitversioning.util.GradleKtsProjectTest.Companion.gradleKtsProjectTest
 import org.eclipse.jgit.api.Git
 import org.gradle.testkit.runner.TaskOutcome
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.*
 import org.junit.jupiter.api.io.CleanupMode.ON_SUCCESS
 import org.junit.jupiter.api.io.TempDir
 import java.io.File
 
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
 class GitVersioningFunctionalTests {
 
-    @Test
-    fun `expect version is not set when not in git repo (kts)`(@TempDir(cleanup = ON_SUCCESS) tempDir: File) {
+    @Nested
+    inner class KtsProject {
 
-        val project = gradleKtsProjectTest(projectDir = tempDir) {
-            buildGradleKts = """
+        @field:TempDir(cleanup = ON_SUCCESS)
+        lateinit var tempDir: File
+
+        private val project: GradleKtsProjectTest by lazy {
+            gradleKtsProjectTest(projectDir = tempDir) {
+                buildGradleKts = """
                 plugins {
                     id("me.qoomon.git-versioning") version "6.3.5"
                 }
@@ -38,61 +46,74 @@ class GitVersioningFunctionalTests {
                     rev { version = "$slashDollar{commit}" }
                 }
             """.trimIndent()
+            }
         }
 
-        val result = project.runner
-            .withArguments("version")
-            .build()
+        @Test
+        @Order(1)
+        fun `expect version is not set when not in git repo`() {
 
-        result.output shouldContain "project is not part of a git repository"
-        result.output shouldContain "0.0.0-SNAPSHOT"
+            val result = project.runner
+                .withArguments("version")
+                .build()
 
-        result.task(":version")?.outcome shouldBe TaskOutcome.SUCCESS
-    }
+            result.output shouldContain "project is not part of a git repository"
+            result.output shouldContain "0.0.0-SNAPSHOT"
 
-    @Test
-    fun `expect version is set when project is in a git repo (kts)`(@TempDir(cleanup = ON_SUCCESS) tempDir: File) {
-
-        val project = gradleKtsProjectTest(projectDir = tempDir) {
-            buildGradleKts = """
-                plugins {
-                    id("me.qoomon.git-versioning") version "6.3.5"
-                }
-                
-                version = "0.0.0-SNAPSHOT"
-                gitVersioning.apply {
-                    refs {
-                        branch(".+") { 
-                            version = "$slashDollar{ref}-SNAPSHOT" 
-                        }
-                        tag("v(?<version>.*)") { 
-                            version = "$slashDollar{ref.version}" 
-                        }
-                    }
-                
-                    // optional fallback configuration in case of no matching ref configuration
-                    rev { version = "$slashDollar{commit}" }
-                }
-            """.trimIndent()
+            result.task(":version")?.outcome shouldBe TaskOutcome.SUCCESS
         }
 
-        Git.init().setDirectory(project.projectDir).call()
+        @Test
+        @Order(2)
+        fun `expect version is set when project is in a git repo`() {
 
-        val result = project.runner
-            .withArguments("version")
-            .build()
+            Git.init().setDirectory(project.projectDir).call()
 
-        result.output shouldNotContain "project is not part of a git repository"
-        result.output shouldContain "master-SNAPSHOT"
+            val result = project.runner
+                .withArguments("version")
+                .build()
 
-        result.task(":version")?.outcome shouldBe TaskOutcome.SUCCESS
+            result.output shouldNotContain "project is not part of a git repository"
+            result.output shouldContain "master-SNAPSHOT"
+
+            result.task(":version")?.outcome shouldBe TaskOutcome.SUCCESS
+        }
+
+        @Test
+        @Order(3)
+        fun `tagged version`() {
+
+            // I'm not sure why init is needed again. It should have already been initialized in the previous test.
+            Git.init().setDirectory(project.projectDir).call()
+
+            Git.open(project.projectDir).apply {
+                add().addFilepattern("build.gradle.kts").call()
+                commit().setMessage("testin'").call()
+                tag().setName("v1.2.3").call()
+
+                println(status().call())
+            }
+
+            val result = project.runner
+                .withArguments("version")
+                .build()
+
+            result.output shouldNotContain "project is not part of a git repository"
+            result.output shouldContain "master-SNAPSHOT"
+
+            result.task(":version")?.outcome shouldBe TaskOutcome.SUCCESS
+        }
     }
 
-    @Test
-    fun `expect version is not set when not in git repo (groovy)`(@TempDir(cleanup = ON_SUCCESS) tempDir: File) {
+    @Nested
+    inner class GroovyProject {
 
-        val project = gradleGroovyProjectTest(projectDir = tempDir) {
-            buildGradle = """
+        @field:TempDir(cleanup = ON_SUCCESS)
+        lateinit var tempDir: File
+
+        private val project: GradleGroovyProjectTest by lazy {
+            gradleGroovyProjectTest(projectDir = tempDir) {
+                buildGradle = """
                 plugins {
                     id("me.qoomon.git-versioning") version "6.3.5"
                 }
@@ -114,55 +135,61 @@ class GitVersioningFunctionalTests {
                     }
                 }
             """.trimIndent()
+            }
         }
 
-        val result = project.runner
-            .withArguments("version")
-            .build()
+        @Test
+        @Order(1)
+        fun `expect version is not set when not in git repo`() {
 
-        result.output shouldContain "project is not part of a git repository"
-        result.output shouldContain "0.0.0-SNAPSHOT"
+            val result = project.runner
+                .withArguments("version")
+                .build()
 
-        result.task(":version")?.outcome shouldBe TaskOutcome.SUCCESS
-    }
+            result.output shouldContain "project is not part of a git repository"
+            result.output shouldContain "0.0.0-SNAPSHOT"
 
-    @Test
-    fun `expect version is set when project is in a git repo (groovy)`(@TempDir(cleanup = ON_SUCCESS) tempDir: File) {
-
-        val project = gradleGroovyProjectTest(projectDir = tempDir) {
-            buildGradle = """
-                plugins {
-                    id("me.qoomon.git-versioning") version "6.3.5"
-                }
-                
-                version = '0.0.0-SNAPSHOT'
-                gitVersioning.apply {
-                    refs {
-                        branch('.+') {
-                            version = '$slashDollar{ref}-SNAPSHOT'
-                        }
-                        tag('v(?<version>.*)') {
-                            version = '$slashDollar{ref.version}'
-                        }
-                    }
-                
-                    // optional fallback configuration in case of no matching ref configuration
-                    rev {
-                        version = '$slashDollar{commit}'
-                    }
-                }
-            """.trimIndent()
+            result.task(":version")?.outcome shouldBe TaskOutcome.SUCCESS
         }
 
-        Git.init().setDirectory(project.projectDir).call()
+        @Test
+        @Order(2)
+        fun `expect version is set to -SNAPSHOT when project is in a git repo without a tag`() {
 
-        val result = project.runner
-            .withArguments("version")
-            .build()
+            Git.init().setDirectory(project.projectDir).call()
 
-        result.output shouldNotContain "project is not part of a git repository"
-        result.output shouldContain "master-SNAPSHOT"
+            val result = project.runner
+                .withArguments("version")
+                .build()
 
-        result.task(":version")?.outcome shouldBe TaskOutcome.SUCCESS
+            result.output shouldNotContain "project is not part of a git repository"
+            result.output shouldContain "master-SNAPSHOT"
+
+            result.task(":version")?.outcome shouldBe TaskOutcome.SUCCESS
+        }
+
+        @Test
+        @Order(3)
+        fun `tagged version`() {
+
+            Git.init().setDirectory(project.projectDir).call()
+
+            Git.open(project.projectDir).apply {
+                add().addFilepattern("build.gradle.kts").call()
+                commit().setMessage("testin'").call()
+                tag().setName("v1.2.3").call()
+
+                println(status().call())
+            }
+
+            val result = project.runner
+                .withArguments("version")
+                .build()
+
+            result.output shouldNotContain "project is not part of a git repository"
+            result.output shouldContain "master-SNAPSHOT"
+
+            result.task(":version")?.outcome shouldBe TaskOutcome.SUCCESS
+        }
     }
 }
